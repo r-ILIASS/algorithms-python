3feabcde5c38b51133069f89a61867964db13be8
r-ILIASS <iliass.root@gmail.com>
2022-02-11 13:22:44 +0100

    (data structures): singly_linked_list

---
+++ Data-structures/singly_linked_list.py
@@ -0,0 +1,34 @@
+class Node:
+    # An object for storing a single object of a linked list.
+    # Models two attributes - data, and the next node in the list.
+
+    data = None
+    next_node = None
+
+    def __init__(self, data):
+        self.data = data
+
+    def __repr__(self):
+        return "<Node data: %s>" % self.data
+
+
+class LinkedList:
+    # Singly linked list
+
+    def __init__(self):
+        self.head = None
+
+    def is_empty(self):
+        return self.head == None
+
+    def size(self):
+        # Returns the number of nodes in the list
+        # Takes 0(n) time
+        current = self.head
+        count = 0
+
+        while current:
+            count += 1
+            current = current.next_node
+
+        return count
